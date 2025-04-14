6068887cdaf871543175cb2b885406239d340b14
Brian <okellobrian18@gmail.com>
2025-04-14 16:44:13 +0300

    Search and Add to list.

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -4,24 +4,40 @@ import Filter from "./Filter";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [itemList, setItemList] = useState(items);
+  const [search, setSearch] = useState("")
   const [selectedCategory, setSelectedCategory] = useState("All");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  const itemsToDisplay = itemList.filter((item) => {
     if (selectedCategory === "All") return true;
-
     return item.category === selectedCategory;
   });
 
+  const searchItems = itemsToDisplay.filter((item) => 
+    item.name.toLowerCase().includes(search.toLowerCase())  
+  )
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const handleItemFormSubmit = (newItem) => {
+    setItemList([...itemList, newItem])
+  }
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleItemFormSubmit}/>
+      <Filter 
+        onCategoryChange={handleCategoryChange} 
+        search={search} 
+        onSearchChange={handleSearch}
+      />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {searchItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
@@ -30,3 +46,4 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
